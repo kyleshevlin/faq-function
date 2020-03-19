3999223be3f46e589270f85c4ed2a25bfce5f5c5
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2020-03-19 08:39:51 -0700

    make body json, add query param

--- functions/faq.js
+++ functions/faq.js
@@ -1,10 +1,22 @@
-exports.handler = function(event, context, callback) {
-  callback(null, {
+const ANSWERS = {
+  editor: 'VSCode',
+  font: 'Dank Mono',
+  theme: "Synthwave '84 (no blur)",
+}
+
+function getBody(key) {
+  if (key) {
+    return ANSWERS[key]
+  }
+
+  return ANSWERS
+}
+
+exports.handler = async event => {
+  const key = event.queryStringParameters.key
+
+  return {
     statusCode: 200,
-    body: `
-Editor: VSCode
-Font: Dank Mono
-Theme: Synthwave '84 (no blur)
-`.trim(),
-  })
+    body: JSON.stringify(getBody(key)),
+  }
 }
