b42318efc079552f78ff9307430dc2c5dee78a86
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2020-03-19 09:24:18 -0700

    better naming of answer

--- functions/faq.js
+++ functions/faq.js
@@ -4,13 +4,13 @@ const ANSWERS = {
   theme: "Synthwave '84 (no blur)",
 }
 
-const getBody = key => (key ? ANSWERS[key] : ANSWERS)
+const getAnswer = key => (key ? ANSWERS[key] : ANSWERS)
 
 exports.handler = async event => {
   const { key = '' } = event.queryStringParameters
-  const body = getBody(key)
+  const answer = getAnswer(key)
 
-  if (body === undefined) {
+  if (answer === undefined) {
     return {
       statusCode: 404,
       body: JSON.stringify({
@@ -21,6 +21,6 @@ exports.handler = async event => {
 
   return {
     statusCode: 200,
-    body,
+    body: JSON.stringify(answer),
   }
 }
